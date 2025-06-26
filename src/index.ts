507eab236363ee66f7d4b0964aee27b038b1e616
Zhu Liang <zhuliangg11@gmail.com>
2025-06-26 08:27:09 +0800

    feat: template

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function placeholder() {
+  return "url-to-json-markdown";
+}
---
+++ src/tests/index.test.ts
@@ -0,0 +1,5 @@
+import { placeholder } from "../index.js";
+
+test("placeholder function", () => {
+  expect(placeholder()).toBe("url-to-json-markdown");
+});
